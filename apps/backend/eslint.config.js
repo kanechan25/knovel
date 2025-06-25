dfa94195b0d4a605bd8228f2d03b4bf8ccce0449
kanechan25 <kt.itengineer@gmail.com>
2025-06-25 23:39:50 +0700

    Revert "refactor config backend"
    This reverts commit c14e72f2bae98f90c4c17b4ca91668c014c2947e.

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -1,33 +1,12 @@
-// @ts-check
-
 const js = require('@eslint/js');
 const typescript = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
 
 module.exports = [
   js.configs.recommended,
   {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'commonjs',
-      globals: {
-        require: 'readonly',
-        module: 'readonly',
-        exports: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        console: 'readonly',
-        process: 'readonly',
-        Buffer: 'readonly',
-        global: 'readonly',
-      },
-    },
-  },
-  {
     files: ['**/*.ts'],
     languageOptions: {
-      parser: tsParser,
+      parser: require('@typescript-eslint/parser'),
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: 'module',
@@ -45,7 +24,10 @@ module.exports = [
       '@typescript-eslint': typescript,
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'off',
       'prefer-const': 'error',
